39b78ceb82416108c56a05e050762cc62a787e8a
Scott Crossan <scrott@gmail.com>
2020-08-14 13:09:01 +0100

    Added support for string array in addRoute method type definition

--- small-router.d.ts
+++ small-router.d.ts
@@ -1,23 +1,37 @@
-import http from 'http'
-import { Server } from 'net'
-import { ParsedUrlQuery } from 'querystring';
-import { Fields, Files } from 'formidable'
+import http from "http";
+import { Server } from "net";
+import { ParsedUrlQuery } from "querystring";
+import { Fields, Files } from "formidable";
 
-export type addRouteCB = (req: http.IncomingMessage, res: http.ServerResponse, rawUrl: string, queryString: ParsedUrlQuery) => void
-export type parseDataCB = (err: null | Error, fields: Fields, files: Files) => void
+export type addRouteCB = (
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+  rawUrl: string,
+  queryString: ParsedUrlQuery
+) => void;
+export type parseDataCB = (
+  err: null | Error,
+  fields: Fields,
+  files: Files
+) => void;
 
 export class Router {
-    prefix: string;
-    constructor();
-    addRoute(route: string, cb: addRouteCB): boolean;
-    addAssetPath(asset: string, path: string, overWrite?: boolean): boolean;
-    route(req: http.IncomingMessage, res: http.ServerResponse): void;
-    listen(port: number, cb: typeof Server.listen): void;
-    renderAsset(path: string, file: string, assetType: string, res: http.ServerResponse): void;
-    parseData(req: http.IncomingHttpHeaders, cb: parseDataCB): void;
-    parseURLParameter(urlPart: string, routePart: string): string | false;
-    pageNotFound(res: http.ServerResponse, url: string): void;
-    close(): void;
+  prefix: string;
+  constructor();
+  addRoute(route: string | string[], cb: addRouteCB): boolean;
+  addAssetPath(asset: string, path: string, overWrite?: boolean): boolean;
+  route(req: http.IncomingMessage, res: http.ServerResponse): void;
+  listen(port: number, cb: typeof Server.listen): void;
+  renderAsset(
+    path: string,
+    file: string,
+    assetType: string,
+    res: http.ServerResponse
+  ): void;
+  parseData(req: http.IncomingHttpHeaders, cb: parseDataCB): void;
+  parseURLParameter(urlPart: string, routePart: string): string | false;
+  pageNotFound(res: http.ServerResponse, url: string): void;
+  close(): void;
 }
 
-export default function smallRouter(http: typeof import('http')): Router;
+export default function smallRouter(http: typeof import("http")): Router;
